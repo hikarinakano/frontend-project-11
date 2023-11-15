5fc83b3d5741ae0947103ae2048e7a069b3cddde
Irina <hikarinakano95@gmail.com>
2023-11-15 17:47:55 +0100

    Added simple validating logic

--- src/index.js
+++ src/index.js
@@ -1,26 +1,86 @@
 import './styles.scss';
 import 'bootstrap';
-import 'yup';
+import * as yup from 'yup';
+import keyBy from 'lodash';
+import has from 'lodash/has.js';
+import isEmpty from 'lodash/isEmpty.js';
+import onChange from 'on-change';
 
-// const root = document.getElementById('app')
+// const rssAdressExample = "https://ru.hexlet.io/lessons.rss";
+const rssAdressExample = 'http://example.com';
+const rssSchema = yup.string().url('URL must be valid').required()
 
-// data.forEach((element) => {
-//   const item = document.createElement('div');
-//   item.textContent = element;
-//   item.classList.add('item');
+const errorMessages = {
+  network: {
+    error: 'Network problems.Try again.',
+  }
+};
+const validate = (schema) => {
+  try {
+    rssSchema.validateSync(schema, {abortEarly: false});
+    return {};
+  } catch(e) {
+    return keyBy(e.inner,'path');
+  }
+};
 
-//   root.appendChild(item);
-// })
-// import watchedState from '../view.js'
 
-// console.log(watchedState)
-// const render = (container, watchedState) => {
-//   console.log(watchedState)
-// }
+// const render = (elements, initialState) => (path, value, prevValue) => {
+//   console.log('path',path)
+//   console.log('value', value)
+//   console.log('prevValue', prevValue);
+//   console.log('elems', elements);
+//   console.log('initialState', initialState);
 
-// export default () => {
-//  const state = {
 
-//   };
+// // there is an example field already, but it doesn't simply show
+// // there is feedback field that should be added with text content
+// // i need to validate rss from URL(rss usually of xml format, so i
+// // think i need to parse and check if it's valid or if content contains xml)?
+// // what should else be in state?
+// };
 
-// }
+const app = () => {
+  const state = {
+    feeds: [],
+  }
+  const elements = {
+    container: document.querySelector('.container-fluid'),
+    form: document.querySelector('form'),
+    inputEl: document.querySelector('input'),
+    example: document.querySelector('.mt-2'),
+    message: document.querySelector('.feedback'),
+  }
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const link = elements.inputEl.value;
+    if (Object.keys(validate(link)).length === 0) {
+      state.feeds.push({feedValue: link, isValidated: true})
+    }
+    else {
+      state.feeds.push({feedValue: link, isValidated: false, error:'The URL must be valid'});
+    }
+    Object.keys(state.feeds).forEach(feedId => {
+      console.log(state.feeds[feedId].error)
+      if (state.feeds[feedId].error) {
+        elements.inputEl.classList.add('is-invalid');
+        if (elements.message.classList.contains('text-sucess')) {
+          elements.message.classList.remove('text-sucess');
+        }
+        elements.message.classList.add('text-danger');
+        elements.message.textContent = state.feeds[feedId].error;
+      } else {
+        elements.message.classList.remove('text-danger');
+        elements.message.classList.add('text-sucess');
+        elements.message.textContent = 'Sucess!';
+        elements.inputEl.classList.remove('is-invalid');
+        elements.inputEl.value = '';
+        elements.inputEl.focus();
+      }
+
+    })
+  })
+
+};
+
+app();
