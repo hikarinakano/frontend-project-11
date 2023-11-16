abd25a242eeba673f11918480f5b9f7de70b0d36
Irina <hikarinakano95@gmail.com>
2023-11-16 17:21:34 +0100

    Linter fix and doubles finding logic

--- src/index.js
+++ src/index.js
@@ -2,28 +2,27 @@ import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
 import keyBy from 'lodash';
-import has from 'lodash/has.js';
-import isEmpty from 'lodash/isEmpty.js';
-import onChange from 'on-change';
+// import has from 'lodash/has.js';
+// import isEmpty from 'lodash/isEmpty.js';
+// import onChange from 'on-change';
 
 // const rssAdressExample = "https://ru.hexlet.io/lessons.rss";
-const rssAdressExample = 'http://example.com';
-const rssSchema = yup.string().url('URL must be valid').required()
+// const rssAdressExample = 'http://example.com';
+const rssSchema = yup.string().url('URL must be valid').required();
 
-const errorMessages = {
-  network: {
-    error: 'Network problems.Try again.',
-  }
-};
+// const errorMessages = {
+//   network: {
+//     error: 'Network problems.Try again.',
+//   }
+// };
 const validate = (schema) => {
   try {
-    rssSchema.validateSync(schema, {abortEarly: false});
+    rssSchema.validateSync(schema, { abortEarly: false });
     return {};
-  } catch(e) {
-    return keyBy(e.inner,'path');
+  } catch (e) {
+    return keyBy(e.inner, 'path');
   }
 };
-
 
 // const render = (elements, initialState) => (path, value, prevValue) => {
 //   console.log('path',path)
@@ -31,7 +30,6 @@ const validate = (schema) => {
 //   console.log('prevValue', prevValue);
 //   console.log('elems', elements);
 //   console.log('initialState', initialState);
-
 
 // // there is an example field already, but it doesn't simply show
 // // there is feedback field that should be added with text content
@@ -43,30 +41,27 @@ const validate = (schema) => {
 const app = () => {
   const state = {
     feeds: [],
-  }
+  };
   const elements = {
     container: document.querySelector('.container-fluid'),
     form: document.querySelector('form'),
     inputEl: document.querySelector('input'),
     example: document.querySelector('.mt-2'),
     message: document.querySelector('.feedback'),
-  }
+  };
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
     const link = elements.inputEl.value;
-    const existedFeed  = state.feeds.find((feed) => feed.feedValue === link && feed.isValidated);
-    console.log(existedFeed)
-    
+    const existedFeed = state.feeds.find((feed) => feed.feedValue === link && feed.isValidated);
+
     if (Object.keys(validate(link)).length === 0 && existedFeed === undefined) {
-      state.feeds.push({feedValue: link, isValidated: true});
-    }else if(Object.keys(validate(link)).length === 0 && existedFeed !== undefined) {
-      state.feeds.push({error: 'RSS already exists'});
+      state.feeds.push({ feedValue: link, isValidated: true });
+    } else if (Object.keys(validate(link)).length === 0 && existedFeed !== undefined) {
+      state.feeds.push({ error: 'RSS already exists' });
+    } else {
+      state.feeds.push({ feedValue: link, isValidated: false, error: 'The URL must be valid' });
     }
-    else {
-      state.feeds.push({feedValue: link, isValidated: false, error:'The URL must be valid'});
-    }
-    Object.keys(state.feeds).forEach(feedId => {
-      
+    Object.keys(state.feeds).forEach((feedId) => {
       if (state.feeds[feedId].error) {
         elements.inputEl.classList.add('is-invalid');
         if (elements.message.classList.contains('text-sucess')) {
@@ -82,10 +77,8 @@ const app = () => {
         elements.inputEl.value = '';
         elements.inputEl.focus();
       }
-
-    })
-  })
-
+    });
+  });
 };
 
 app();
