fcef2f24f3a0ba9fe65f15512bc3b27aac203703
Irina <hikarinakano95@gmail.com>
2024-02-07 13:33:46 +0100

    fixed all displaying issues connected with i18next

--- src/view.js
+++ src/view.js
@@ -2,46 +2,23 @@ import onChange from 'on-change';
 import _ from 'lodash';
 // import { uniqueId } from 'lodash';
 
-export default (elements, state) => {
+export default (elements, i18n, state) => {
   const { inputEl, message } = elements;
   return onChange(state, () => {
-    const errorMsg = state.rssForm.errors;
-    console.log('errors', errorMsg);
-    if (_.has(errorMsg, 'url')) {
+    const { errors, fields } = state.rssForm;
+    if (_.has(errors, 'url')) {
+      message.classList.remove('text-success');
+      message.classList.add('text-danger');
       inputEl.classList.add('is-invalid');
-      const warning = errorMsg.url.toString().replace('ValidationError:', '');
-      message.textContent = warning;
+      message.textContent = i18n.t(errors.url.message);
     } else {
+      message.classList.add('text-success');
+      message.classList.remove('text-danger');
       inputEl.classList.remove('is-invalid');
-      inputEl.value = '';
-      message.textContent = '';
+
+      message.textContent = i18n.t('success');
       inputEl.focus();
     }
-    // const existedFeed = state.feeds.find((feed) => feed.feedValue === link && feed.isValidated);
-
-    // if (Object.keys(validate(link)).length === 0 && existedFeed === undefined) {
-    //   state.feeds.push({ feedValue: link, isValidated: true });
-    // } else if (Object.keys(validate(link)).length === 0 && existedFeed !== undefined) {
-    //   state.feeds.push({ error: 'RSS already exists' });
-    // } else {
-    //   state.feeds.push({ feedValue: link, isValidated: false, error: 'URL must be valid' });
-    // }
-    // Object.keys(state.feeds).forEach((feedId) => {
-    //   if (state.feeds[feedId].error) {
-    //     elements.inputEl.classList.add('is-invalid');
-    //     if (elements.message.classList.contains('text-sucess')) {
-    //       elements.message.classList.remove('text-sucess');
-    //     }
-    //     elements.message.classList.add('text-danger');
-    //     elements.message.textContent = state.feeds[feedId].error;
-    //   } else {
-    //     elements.message.classList.remove('text-danger');
-    //     elements.message.classList.add('text-sucess');
-    //     elements.message.textContent = 'Sucess!';
-    //     elements.inputEl.classList.remove('is-invalid');
-    //     elements.inputEl.value = '';
-    //     elements.inputEl.focus();
-    //   }
-    // });
+    inputEl.value = fields.url;
   });
 };
