9a6f0c8c62cb79f9fdf04ae17cb44629678294c3
Irina <hikarinakano95@gmail.com>
2024-02-07 13:33:14 +0100

    Fixed issue with error message correctly appearing with i18next

--- src/index.js
+++ src/index.js
@@ -2,30 +2,33 @@ import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
 import _ from 'lodash';
+import i18next from 'i18next';
 import watch from './view.js';
+import resources from './locales/index.js';
+
+yup.setLocale({
+  mixed: {
+    default: 'Must not happen',
+    required: ({ path }) => ({ key: 'networkError', values: { path } }),
+  },
+  string: {
+    url: ({ path }) => ({ key: 'notUrl', values: { path } }),
+    test: ({ path }) => ({ key: 'duplicateUrl', values: { path } }),
+  },
+});
 
 // const rssAdressExample = "https://ru.hexlet.io/lessons.rss";
 // const rssAdressExample = 'http://example.com';
-
+// add to state {feeds:[{feedId, url, posts: [{postId, text}]}]}
 const rssSchema = yup.object().shape({
   url: yup.string()
-    .url('URL must be valid')
-    .test('uniqie-url', 'This URL has already been added', function feedsExclude(value) {
+    .url('errors.validation.notUrl')
+    .test('uniqie-url', 'errors.validation.duplicateUrl', function feedsExclude(value) {
       const { feeds } = this.options.context;
       return !feeds.includes(value);
     })
     .required(),
 });
-
-// const validate = (fields, { context, abortEarly }) => {
-//   try {
-//     rssSchema.validateSync(fields, { context, abortEarly });
-//     return {};
-//   } catch (e) {
-//     console.log(e.inner);
-//     return _.keyBy(e.inner, 'path');
-//   }
-// };
 
 const validate = (state) => rssSchema.validate(
   state.rssForm.fields,
@@ -40,8 +43,14 @@ const elements = {
   message: document.querySelector('.feedback'),
 };
 
-const app = () => {
-  const state = watch(elements, {
+const app = async () => {
+  const i18nInstance = i18next.createInstance();
+  await i18nInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  });
+  const state = watch(elements, i18nInstance, {
     rssForm: {
       fields: {
         url: '',
@@ -50,15 +59,16 @@ const app = () => {
     },
     feeds: [],
   });
-
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
+
     const link = elements.inputEl.value;
     state.rssForm.fields.url = link;
     validate(state)
       .then(() => {
         state.rssForm.errors = {};
         state.feeds.push(state.rssForm.fields.url);
+        state.rssForm.fields.url = '';
       })
       .catch((err) => {
         state.rssForm.errors = _.keyBy(err.inner, 'path');
