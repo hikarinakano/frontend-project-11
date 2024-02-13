44f7e589ba8fc363f42dae5e20b26d20b057a4c5
Irina <hikarinakano95@gmail.com>
2024-02-13 15:56:08 +0100

    Linter fix

--- src/view.js
+++ src/view.js
@@ -1,24 +1,40 @@
 import onChange from 'on-change';
 import _ from 'lodash';
+import render from './render.js';
 // import { uniqueId } from 'lodash';
 
 export default (elements, i18n, state) => {
   const { inputEl, message } = elements;
+  const [postsTr, feedsTr, viewButton] = [i18n.t('posts'), i18n.t('feeds'), i18n.t('viewButton')];
   return onChange(state, () => {
-    const { errors, fields } = state.rssForm;
+    const {
+      rssForm: { errors },
+      rssFeeds,
+    } = state;
+    // console.log(Object.entries(errors).length)
     if (_.has(errors, 'url')) {
       message.classList.remove('text-success');
       message.classList.add('text-danger');
       inputEl.classList.add('is-invalid');
       message.textContent = i18n.t(errors.url.message);
-    } else {
-      message.classList.add('text-success');
-      message.classList.remove('text-danger');
-      inputEl.classList.remove('is-invalid');
-
-      message.textContent = i18n.t('success');
-      inputEl.focus();
     }
-    inputEl.value = fields.url;
+    if (_.has(errors, 'parseError')) {
+      message.classList.remove('text-success');
+      message.classList.add('text-danger');
+      inputEl.classList.add('is-invalid');
+      message.textContent = i18n.t('errors.noRssFound');
+    }
+
+    if (state.rssFeeds.length !== 0) {
+      inputEl.focus();
+      // render of feeds and posts on success
+      render(rssFeeds, [postsTr, feedsTr, viewButton]);
+      // message.classList.add('text-success');
+      // message.classList.remove('text-danger');
+      // inputEl.classList.remove('is-invalid');
+      // message.textContent = i18n.t('success');
+      // inputEl.value = fields.url;
+      // // inputEl.value = '';
+    }
   });
 };
