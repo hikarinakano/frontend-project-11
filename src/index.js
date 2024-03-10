6892e6acaf02cb29184d704126e5a5a3e59aab0b
Irina <hikarinakano95@gmail.com>
2024-03-10 14:32:03 +0100

    Linter fixes and also changed logic of working with Promises of loadingRss, checking new posts and clicked links

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import i18next from 'i18next';
 import axios from 'axios';
 import watch from './view.js';
 import resources from './locales/index.js';
-import parse from './parser.js';
-import parseRssXml from './rssParser.js';
+import parse from './rssParser.js';
 
 yup.setLocale({
   mixed: {
@@ -20,8 +19,6 @@ yup.setLocale({
   },
 });
 
-// const rssAdressExample = "https://ru.hexlet.io/lessons.rss";
-// const rssAdressExample = 'http://example.com';
 const rssSchema = yup.object().shape({
   url: yup.string()
     .url('errors.validation.notUrl')
@@ -44,25 +41,26 @@ const elements = {
   message: document.querySelector('.feedback'),
 };
 
-const getRequest = (url) => axios.get(
-  `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`,
-)
-  .then((response) => {
-    const data = response.data.contents;
-    return parse(data);
-  })
-  .catch((err) => {
-    console.error('Error occured', err.message);
-  });
+// const getRequest = (url) => axios.get(
+//   `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`,
+// )
+//   .then((response) => {
+//     const data = response.data.contents;
+//     return parse(data);
+//   })
+//   .catch((err) => {
+//     state.rssForm.errors = { networkError: err.message }
+//     console.error('Error occured', err.message);
+//   });
 
-function checkAndAddNewPosts(newPosts, existingPosts) {
-  const existingLinks = new Set(existingPosts.map((post) => post.id));
+function checkAndAddNewPosts(newPosts, posts) {
+  const existingLinks = new Set(posts.map((post) => post.id));
   newPosts.forEach((post) => {
     if (!existingLinks.has(post.id)) {
-      existingPosts.unshift(post);
+      posts.unshift(post);
     }
   });
-  return existingPosts;
+  return posts;
 }
 
 // const loadRss = (url, rssFeeds) => getRequest(url)
@@ -90,6 +88,7 @@ function checkAndAddNewPosts(newPosts, existingPosts) {
 //   });
 
 const app = async () => {
+  console.log('app call', new Error().stack);
   const i18nInstance = i18next.createInstance();
   await i18nInstance.init({
     lng: 'ru',
@@ -110,6 +109,7 @@ const app = async () => {
   });
 
   const updateStateLinks = (links) => {
+    console.log('UpdateStateLinks call', new Error().stack);
     links.forEach((link) => {
       const { children } = link;
       Array.from(children).forEach((child) => {
@@ -123,36 +123,47 @@ const app = async () => {
       });
     });
   };
-  const loadRss = (url, rssFeeds) => getRequest(url)
-    .then((parsedData) => {
-      const index = _.findIndex(rssFeeds, (feed) => feed.id === url);
-      const rssFeed = parseRssXml(parsedData, url);
-      if (index < 0) {
-        state.rssFeeds = [rssFeed, ...rssFeeds];
-      } else {
-        // const existingFeed = state.rssFeeds[index];
-        // state.rssFeeds[index].posts = checkAndAddNewPosts(rssFeed.posts, existingFeed.posts);
-        state.rssFeeds = rssFeeds.map((feed, i) => {
-          if (i === index) {
-            return {
-              ...feed,
-              posts: checkAndAddNewPosts(rssFeed.posts, feed.posts),
-            };
-          }
-          return feed;
-        });
-      }
-    })
-    .catch((e) => {
-      state.rssForm.errors = { parseError: e };
-    });
+
+  const loadRss = (url) => {
+    console.log('getRequest call', new Error().stack);
+    return axios.get(
+      `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`,
+    )
+      .then((response) => {
+        console.log('No Error', new Error().stack);
+        const data = response.data.contents;
+        return parse(data, url);
+      })
+      .then((rssFeed) => {
+        console.log('No Error', new Error().stack);
+        const index = _.findIndex(state.rssFeeds, (feed) => feed.id === url);
+        if (index < 0) {
+          state.rssFeeds = [rssFeed, ...state.rssFeeds];
+        } else {
+          const existingFeed = state.rssFeeds[index];
+          state.rssFeeds[index].posts = checkAndAddNewPosts(rssFeed.posts, existingFeed.posts);
+          // state.rssFeeds = rssFeeds.map((feed, i) => {
+          //   if (i === index) {
+          //     return {
+          //       ...feed,
+          //       posts: checkAndAddNewPosts(rssFeed.posts, feed.posts),
+          //     };
+          //   }
+          //   return feed;
+          // });
+        }
+      });
+  };
+
   const fnc = () => {
-    state.feeds.map((url) => loadRss(url, state));
+    console.log('fnc call', new Error().stack);
+    state.feeds.map((url) => loadRss(url));
     const links = document.querySelectorAll('.list-group-item');
     updateStateLinks(links);
   };
 
   const refresh = () => {
+    console.log('not Error', new Error().stack);
     setTimeout(() => {
       fnc();
       refresh();
@@ -162,24 +173,33 @@ const app = async () => {
   refresh();
 
   elements.form.addEventListener('submit', (e) => {
+    console.log('not Error', new Error().stack);
     e.preventDefault();
     const link = elements.inputEl.value;
 
     state.rssForm.fields.url = link;
     validate(state)
       .then(() => {
+        console.log('not Error', new Error().stack);
         const currentUrl = state.rssForm.fields.url;
-        loadRss(currentUrl, state.rssFeeds)
+        loadRss(currentUrl)
           .then(() => {
             state.rssForm.errors = {};
             state.feeds = [...state.feeds, currentUrl];
             state.rssForm.fields.url = '';
           })
-          .catch((parseError) => {
-            state.rssForm.errors = { parseError };
+          .catch((error) => {
+            console.log(error);
+            if (error.code === 'ERR_NETWORK') {
+              state.rssForm.errors = { networkError: error.message };
+            } else {
+              state.rssForm.errors = { parseError: error };
+            }
+            console.log('No Error', new Error().stack);
           });
       })
       .catch((err) => {
+        console.log('Now error is parseError', new Error().stack);
         state.rssForm.errors = _.keyBy(err.inner, 'path');
       });
   });
