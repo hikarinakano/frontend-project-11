6c364b5ad8e44f199379d05ea448beb6019b83af
Irina <hikarinakano95@gmail.com>
2024-03-15 14:39:41 +0100

    Added logic with emptying the error/success message field and locking the add button while loading(statuses in state change the behaviour)to view.js

--- src/view.js
+++ src/view.js
@@ -2,24 +2,16 @@ import onChange from 'on-change';
 import _ from 'lodash';
 import render from './render.js';
 
-// const changeVisitedLinks = (openedLinks) => {
-//   const links = document.querySelectorAll('a[data-id].fw-bold');
-//   Array.from(links).forEach((link) => {
-//     const url = link.getAttribute('href');
-//     if (openedLinks.has(url)) {
-//       link.classList.remove('fw-bold');
-//       link.classList.add('fw-normal', 'link-secondary');
-//     }
-//   });
-// };
-
 export default (elements, i18n, state) => {
-  const { inputEl, message } = elements;
+  const { inputEl, message, submitBtn } = elements;
   const [postsTr, feedsTr, viewButton] = [i18n.t('posts'), i18n.t('feeds'), i18n.t('viewButton')];
   return onChange(state, () => {
     const {
       rssForm: { errors, fields },
     } = state;
+    console.error('errors', state.rssForm.errors);
+    console.log('status', state.rssForm.status);
+    submitBtn.disabled = false;
     if (_.has(errors, ['url'])) {
       message.classList.remove('text-success');
       message.classList.add('text-danger');
@@ -27,28 +19,49 @@ export default (elements, i18n, state) => {
       message.textContent = i18n.t(errors.url.message);
     }
     if (_.has(errors, ['networkError'])) {
-      message.classList.remove('text-success');
-      message.classList.add('text-danger');
-      inputEl.classList.add('is-invalid');
-      message.textContent = i18n.t('errors.networkError');
+      if (state.rssForm.status === 'loading Rss') {
+        message.innerHtml = '';
+        submitBtn.disabled = true;
+        inputEl.classList.remove('is-invalid');
+      }
+      if (state.rssForm.status === 'not loading') {
+        message.classList.remove('text-success');
+        message.classList.add('text-danger');
+        inputEl.classList.add('is-invalid');
+        message.textContent = i18n.t('errors.networkError');
+      }
     }
-    if (_.has(errors, ['parseError'])) {
-      message.classList.remove('text-success');
-      message.classList.add('text-danger');
-      inputEl.classList.add('is-invalid');
-      message.textContent = i18n.t('errors.noRssFound');
-    }
-    if (_.isEmpty(errors)) {
-      inputEl.focus();
-      // render of feeds and posts on success
-      render(state, [postsTr, feedsTr, viewButton]);
 
-      message.classList.add('text-success');
-      message.classList.remove('text-danger');
-      inputEl.classList.remove('is-invalid');
-      message.textContent = i18n.t('success');
+    if (_.has(errors, ['parseError'])) {
+      if (state.rssForm.status === 'loading Rss') {
+        inputEl.classList.remove('is-invalid');
+        message.textContent = '';
+        submitBtn.disabled = true;
+      }
+      if (state.rssForm.status === 'not loading') {
+        message.classList.remove('text-success');
+        message.classList.add('text-danger');
+        inputEl.classList.add('is-invalid');
+        message.textContent = i18n.t('errors.noRssFound');
+      }
+    }
+
+    if (_.isEmpty(errors)) {
+      if (state.rssForm.status === 'loading Rss') {
+        inputEl.classList.remove('is-invalid');
+        message.textContent = '';
+        submitBtn.disabled = true;
+      }
+      if (state.rssForm.status === 'success') {
+        inputEl.focus();
+        render(state, [postsTr, feedsTr, viewButton]);
+
+        message.classList.add('text-success');
+        message.classList.remove('text-danger');
+        inputEl.classList.remove('is-invalid');
+        message.textContent = i18n.t('success');
+      }
     }
     inputEl.value = fields.url;
-    // changeVisitedLinks(openedLinks);
   });
 };
