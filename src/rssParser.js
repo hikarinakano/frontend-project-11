cc87afb6f9952442073ba34eab8d41bb5bf4e4d2
Irina <hikarinakano95@gmail.com>
2024-07-31 15:38:43 +0200

    removed the adding of url and id both in parsed feeds and posts

--- src/rssParser.js
+++ src/rssParser.js
@@ -1,35 +1,36 @@
+import _ from 'lodash';
+
+
 const parsePostsXml = (posts) => [...posts].map((item) => {
   const title = item.querySelector('title').textContent;
   const desc = item.querySelector('description').textContent;
-  const link = item.querySelector('link').textContent;
+  // const postId = _.uniqueId();
+  // const url = item.querySelector('link').textContent;
   return {
     title,
     desc,
-    id: link,
   };
 });
 
-const parseRssXml = (xml, url) => {
+const parseRssXml = (xml) => {
   const feed = xml.querySelector('channel');
   const posts = feed.querySelectorAll('item');
   const feedTitle = feed.querySelector('title').textContent;
   const feedDesc = feed.querySelector('description').textContent;
-
   return {
-    id: url,
     feedTitle,
     feedDesc,
-    posts: parsePostsXml(posts),
+    posts: parsePostsXml(posts, feedId),
   };
 };
 
-const parseFeed = (data, url) => {
+const parseFeed = (data) => {
   const parser = new DOMParser();
   const xml = parser.parseFromString(data, 'text/xml');
   if (xml.querySelector('parsererror')) {
     throw new Error('parseError');
   }
-  return parseRssXml(xml, url);
+  return parseRssXml(xml);
 };
 
 export default parseFeed;
