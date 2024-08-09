b228ec819285e1912db4444e7ce7d138b9fee86a
Irina <hikarinakano95@gmail.com>
2024-08-09 16:11:36 +0200

    removed the mixed custom errors, as it was not used in main app

--- src/locales/yupLocale.js
+++ src/locales/yupLocale.js
@@ -1,8 +1,4 @@
 const customErrors = {
-  mixed: {
-    default: 'Must not happen',
-    required: ({ path }) => ({ key: 'networkError', values: { path } }),
-  },
   string: {
     url: ({ path }) => ({ key: 'notUrl', values: { path } }),
     test: ({ path }) => ({ key: 'duplicateUrl', values: { path } }),
